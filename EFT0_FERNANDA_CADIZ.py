80485aca37b076b70a375c4ee67bed3d4520159f
fer-cadiz <fer.cadiz@duocuc.cl>
2026-07-06 02:17:59 -0400

    Tercer commit: opcion 1 agregada y funcionando

--- EFT0_FERNANDA_CADIZ.py
+++ EFT0_FERNANDA_CADIZ.py
@@ -15,7 +15,7 @@ def leer_opcion():
     while True:
         try:
             opcion = int(input(">"))
-            if 1 > opcion > 7:
+            if 1 <= opcion <= 7:
                 return opcion
             else:
                 print("Error, opcion invalida")
@@ -48,12 +48,38 @@ def validar_stock(stock):
 def validar_vendidos(vendidos):
     return vendidos >= 0
 
+def stock_categoria(categoria,productos, inventario):
+    total_stock = 0
+    encontrado = False
+    for codigo in productos:
+        categoria_producto = productos[codigo][1]
+        if categoria_producto.lower() == categoria.lower():
+            encontrado = True
+            stock_producto = inventario[codigo][0]
+            total_stock += stock_producto
+    if encontrado == False:
+        print("Categoria no encontrada")
+    else:
+        print(f"Stock total: {total_stock}")
+
+def ejecutar_stock_categoria(productos, inventario):
+    while True:
+        categoria = input("Ingrese categoria:\n>").strip()
+        if validar_categoria(categoria) == False:
+            print("Categoria no puede estart vacia.")
+        else:
+            break
+    stock_categoria(categoria, productos, inventario)
+
 def main():
+    productos = {}
+    inventario = {}
+
     while True:
         mostrar_menu()
         opcion = leer_opcion()
         if opcion == 1:
-            pass
+            ejecutar_stock_categoria(productos, inventario)
         elif opcion == 2:
             pass
         elif opcion == 3:
@@ -66,3 +92,5 @@ def main():
             pass
         elif opcion == 7:
             pass
+
+main()
